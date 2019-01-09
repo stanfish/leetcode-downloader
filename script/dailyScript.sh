e94c3a9fffa4811f32b88c3a41192c2f125dd77e
Stanley Yu <stanfish@gmail.com>
2019-01-08 22:09:12 -0800

    add daily script for auto update question and submission

--- script/dailyScript.sh
+++ script/dailyScript.sh
@@ -1,5 +1,9 @@
-cd /home/pi/code/leetcode-downloader/
-
+#cd /home/pi/code/leetcode-downloader/
+#cd /PATH/leetcode-downloader/
+git checkout develop
 node fetchSubmission.js
 node fetchQuestion.js
 node mergeQA.js
+git add .
+git commit -m "Problems and submission update $(date +'%D')"
+git push origin develop
